29c8c5ce5b23bfa08fec08228f4438c7ab90ffdc
rexzea <satriabam01@gmail.com>
2025-01-03 10:47:59 +0700

    Update tes4.py

--- filter/tes4.py
+++ filter/tes4.py
@@ -4,7 +4,7 @@ import numpy as np
 import math
 from datetime import datetime
 
-class hai:
+class RexzeaFilterFaceDetector:
     def __init__(self):
         self.mp_face_mesh = mp.solutions.face_mesh
         self.mp_drawing = mp.solutions.drawing_utils
@@ -319,7 +319,7 @@ class hai:
         cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    detector = hai()
+    detector = RexzeaFilterFaceDetector()
     detector.start_webcam()
 
 
